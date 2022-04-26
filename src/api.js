cf53588471c2da1db0829fe5d5bf85cc753b1e5e
Moises De Leon <mdeleon5826@gmail.com>
2022-04-26 17:18:30 -0400

    added parameters to deploy with Netlify

---
+++ src/api.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+router.get('/test',(req,res) => {
+    res.json({
+        hello: "test!"
+      });
+
+})
+
+router.post('/testpost',(req,res) => {
+    res.json({
+        hello: "hit the POST!"
+      });
+})
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
